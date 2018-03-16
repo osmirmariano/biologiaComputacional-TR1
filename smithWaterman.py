1c308c650edfe2a2fcb9ba9ede9c4fece2b6dae0
Osmir <osmirmariano@uft.edu.br>
2018-03-16 12:12:04 -0300

    Criando as primeiras regras dos algoritmos

--- smithWaterman.py
+++ smithWaterman.py
@@ -1,1 +1,24 @@
-#Algoritmo Smith Waterman
+#coding: utf-8
+import sys
+
+class AlinhamentoSmithWaterman():
+    #Caracteres iguais
+    match = 1
+    #Caracteres diferentes
+    mismatch = -1
+    #Lacunas
+    gap = -1
+    
+    sequencia1 = raw_input('Forneça a primeira sequência: ')
+    sequencia2 = raw_input('Forneça a segunda sequência: ')
+    
+    matrix = []
+    for i in range(len(sequencia1)):
+        matrix.append([len(sequencia1)]*len(sequencia2))
+        for j in range(len(sequencia2)):
+            if(i == 0 or j == 0):
+                matrix[i][j] = 0
+            else:
+                matrix[i][j] = 1
+            print(matrix[i][j]) 
+        print('\n') 
